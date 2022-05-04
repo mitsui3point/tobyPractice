78044400bf1c796783d1fa43781d2155c0575ced
mitsui3point <rladbsgh0331@gmail.com>
2022-05-04 16:04:54 +0900

    cross-cutting concerns

---
+++ src/main/java/springbook/user/test/Test.java
@@ -0,0 +1,26 @@
+package springbook.user.test;
+
+import springbook.user.dao.UserDao;
+import springbook.user.domain.User;
+
+import java.sql.SQLException;
+
+public class Test {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao dao = new UserDao();
+        User user = new User();
+        user.setId("yhkim");
+        user.setName("김윤호");
+        user.setPassword("notmerried");
+
+        dao.add(user);
+
+        System.out.println(user.getId() + "등록 성공");
+
+        User user2 = dao.get(user.getId());
+
+        System.out.println(user2.getName());
+        System.out.println(user2.getPassword());
+        System.out.println(user2.getId() + "조회 성공");
+    }
+}
