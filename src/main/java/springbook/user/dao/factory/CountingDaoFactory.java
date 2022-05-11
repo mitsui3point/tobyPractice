e1606b1c4ce4f906024ae7f31397620bafe5153e
mitsui3point <rladbsgh0331@gmail.com>
2022-05-11 10:26:10 +0900

    1.7.4. @Configuration, @Bean 애노테이션을 활용하여 싱글톤패턴을 적용하여 메모리에 올라간 UserDao 의 reference 와 new 를 활용하여 만들어진 UserDao 의 reference 비교

--- src/main/java/springbook/user/dao/factory/CountingDaoFactory.java
+++ src/main/java/springbook/user/dao/factory/CountingDaoFactory.java
@@ -2,37 +2,24 @@ package springbook.user.dao.factory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springbook.user.dao.AccountDao;
-import springbook.user.dao.MessageDao;
-import springbook.user.dao.UserDao;
 import springbook.user.dao.connection.ConnectionMaker;
 import springbook.user.dao.connection.counting.CountingConnectionMaker;
 import springbook.user.dao.connection.daum.DConnectionMaker;
 
-@Configuration // application context OR bean factory 가 사용할 설정정보라는 표시
+
+@Configuration
 public class CountingDaoFactory {
-    @Bean // 오브젝트 생성을 담당하는 IoC 용 메소드라는 표시
-    public UserDao userDao() {
-        return new UserDao(this.connectionMaker());
-    }
-    @Bean
-    public AccountDao accountDao() {
-        return new AccountDao(this.connectionMaker());
-    }
-    @Bean
-    public MessageDao messageDao() {
-        return new MessageDao(this.connectionMaker());
-    }
     @Bean
     public ConnectionMaker connectionMaker() {
-//        System.out.println("this.realConnectionMaker() == realConnectionMaker() :" + (this.realConnectionMaker() == realConnectionMaker()));
-        return new CountingConnectionMaker(this.realConnectionMaker());
+        ConnectionMaker realConnMaker = this.realConnectionMaker();
+        System.out.println("this");
+        System.out.println(this);
+        System.out.println("inner realConnectionMaker");
+        System.out.println(realConnMaker);
+        return new CountingConnectionMaker(realConnMaker);
     }
     @Bean
     public ConnectionMaker realConnectionMaker() {
         return new DConnectionMaker();
-//        return new NConnectionMaker();
-//        return new ProductionDBConnectionMaker();
-//        return new LocalDBConnectionMaker();
     }
 }
