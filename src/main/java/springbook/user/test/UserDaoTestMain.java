a6b23126e22b094621cdb523ecd14435b8c823c5
mitsui3point <rladbsgh0331@gmail.com>
2022-05-17 01:00:53 +0900

    2.3.1. JUnit 테스트 실행 방법(IDE 활용; JUnitCore 없이 실행)

--- src/main/java/springbook/user/test/UserDaoTestMain.java
+++
@@ -1,9 +0,0 @@
-package springbook.user.test;
-
-import org.junit.runner.JUnitCore;
-
-public class UserDaoTestMain {
-    public static void main(String[] args) {
-        JUnitCore.main("springbook.user.test.UserDaoTest");
-    }
-}
