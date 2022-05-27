2f5b58333a354ab4cdabc13c9271c53a529ec65e
mitsui3point <rladbsgh0331@gmail.com>
2022-05-28 02:58:58 +0900

    2.3.3. 포괄적인 테스트(addAndGet() 테스트 보완)

--- src/main/java/springbook/user/test/UserDaoTest.java
+++ src/main/java/springbook/user/test/UserDaoTest.java
@@ -21,15 +21,20 @@ public class UserDaoTest {
         dao.deleteAll();
         assertThat(dao.getCount(), is(0));
 
-        User user = new User("yhkim2", "김윤호2", "notmerried");
+        User user1 = new User("yhkim01", "김윤호1", "springno1");
+        User user2 = new User("yhkim02", "김윤호2", "springno2");
 
-        dao.add(user);
-        assertThat(dao.getCount(), is(1));
+        dao.add(user1);
+        dao.add(user2);
+        assertThat(dao.getCount(), is(2));
 
-        User user2 = dao.get(user.getId());
+        User userget1 = dao.get(user1.getId());
+        assertThat(userget1.getName(), is(user1.getName()));
+        assertThat(userget1.getPassword(), is(user1.getPassword()));
 
-        assertThat(user2.getName(), is(user.getName()));
-        assertThat(user2.getPassword(), is(user.getPassword()));
+        User userget2 = dao.get(user2.getId());
+        assertThat(userget2.getName(), is(user2.getName()));
+        assertThat(userget2.getPassword(), is(user2.getPassword()));
     }
 
     @Test
