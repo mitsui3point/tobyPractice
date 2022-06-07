41ee321b0a9350ea419ce98ab4eed0014e3f47bb
mitsui3point <rladbsgh0331@gmail.com>
2022-06-08 00:40:31 +0900

    1.8.4. 프로퍼티 값의 주입(ConnectionMaker.java interface삭제)

--- src/main/java/springbook/user/dao/connection/ConnectionMaker.java
+++
@@ -1,8 +0,0 @@
-package springbook.user.dao.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionMaker {
-    public Connection makeConnection() throws ClassNotFoundException, SQLException;
-}
