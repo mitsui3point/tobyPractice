e6ac4a6582227ca5dd1020d9767581acaeb7e01c
mitsui3point <rladbsgh0331@gmail.com>
2022-06-12 01:42:29 +0900

    3.1.1. 예외처리 기능을 갖춘 DAO(JDBC 수정 기능의 예외처리 코드)

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -59,13 +59,29 @@ public class UserDao {
         return user;
     }
     public void deleteAll() throws SQLException {
-        Connection c = this.dataSource.getConnection();
-        PreparedStatement ps = c.prepareStatement(
-                "delete from users");
-        ps.executeUpdate();
-
-        ps.close();
-        c.close();
+        Connection c = null;
+        PreparedStatement ps = null;
+        try {
+            c = this.dataSource.getConnection();
+            ps = c.prepareStatement(
+                    "delete from users");
+            ps.executeUpdate(); // 여기서 예외가 발생하면 바로 메소드 실행이 중단된다.
+        } catch (SQLException e) {
+            throw e; // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다. 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
+        } finally { // finally 이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
+            if(ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e){ // ps.close()  메소드에서도 SQLException 이 발생할 수 있기 때문에 이를 잡아줘야 한다. 그렇지 않으면 Connection 을 close() 하지 못하고 메소드를 빠져나갈 수 있다.
+                }
+            }
+            if(c != null) {
+                try {
+                    c.close(); // Connection 반환
+                } catch (SQLException e){
+                }
+            }
+        }
     }
 
     public int getCount() throws SQLException {
