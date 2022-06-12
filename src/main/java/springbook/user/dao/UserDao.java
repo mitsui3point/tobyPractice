6d0162553138da6e696a4a6eb6f7834eb5e71a8d
mitsui3point <rladbsgh0331@gmail.com>
2022-06-13 00:32:21 +0900

    3.2.2. 분리와 재사용을 위한 디자인 패턴 적용(DI 적용을 위한 클라이언트/컨텍스트 분리)

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -59,31 +59,35 @@ public class UserDao {
 
         return user;
     }
+
+    /**
+     * 클라이언트 책임을 담당할 deleteAll() 메소드
+     * @throws SQLException
+     */
     public void deleteAll() throws SQLException {
+        StatementStrategy st = new DeleteAllStatement(); // 선정한 전략 클래스의 오브젝트 생성
+        jdbcContextWithStatementStrategy(st); // 컨텍스트 호출. 전략 오브젝트 전달
+    }
+
+    /**
+     * 메소드로 분리한 try/catch/finally 컨텍스트 코드
+     * @param stmt  클라이언트가 컨텍스트를 호출할 때 넘겨줄 전략 파라미터
+     * @throws SQLException
+     */
+    private void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
         Connection c = null;
         PreparedStatement ps = null;
         try {
             c = this.dataSource.getConnection();
 
-            StatementStrategy strategy = new DeleteAllStatement();
-            ps = strategy.makePreparedStatement(c);
-            
+            ps = stmt.makePreparedStatement(c);
+
             ps.executeUpdate(); // 여기서 예외가 발생하면 바로 메소드 실행이 중단된다.
         } catch (SQLException e) {
             throw e; // 예외가 발생했을 때 부가적인 작업을 해줄 수 있도록 catch 블록을 둔다. 아직은 예외를 다시 메소드 밖으로 던지는 것밖에 없다.
         } finally { // finally 이므로 try 블록에서 예외가 발생했을 때나 안 했을 때나 모두 실행된다.
-            if(ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e){ // ps.close()  메소드에서도 SQLException 이 발생할 수 있기 때문에 이를 잡아줘야 한다. 그렇지 않으면 Connection 을 close() 하지 못하고 메소드를 빠져나갈 수 있다.
-                }
-            }
-            if(c != null) {
-                try {
-                    c.close(); // Connection 반환
-                } catch (SQLException e){
-                }
-            }
+            if(ps != null) try { ps.close(); } catch (SQLException e){} // ps.close()  메소드에서도 SQLException 이 발생할 수 있기 때문에 이를 잡아줘야 한다. 그렇지 않으면 Connection 을 close() 하지 못하고 메소드를 빠져나갈 수 있다.
+            if(c != null) try { c.close();} catch (SQLException e){} // Connection 반환
         }
     }
 
