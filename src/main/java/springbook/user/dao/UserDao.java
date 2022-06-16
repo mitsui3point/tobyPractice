c724b44e706c13aca22706ef73a8913d14c4b061
mitsui3point <rladbsgh0331@gmail.com>
2022-06-16 20:05:55 +0900

    3.5.2. 편리한 콜백의 재활용(콜백의 분리와 재활용)

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -41,6 +41,7 @@ public class UserDao {
                     PreparedStatement ps = c.prepareStatement(
                             "insert into users(id, name, password) values(?, ?, ?)");
                     // 로컬(내부) 클래스의 코드에서 외부의 메소드 로컬 변수에 직접 접근할 수 있다. 단 외부의 메소드 로컬 변수를 사용하려면 외부 메소드 argument 에 final을 붙여주어야 한다.
+                    // user 파라미터는 메소드 내부에서 변경될 일이 없으므로 final로 선언해도 무방하다.
                     ps.setString(1, user.getId());
                     ps.setString(2, user.getName());
                     ps.setString(3, user.getPassword());
@@ -86,11 +87,16 @@ public class UserDao {
      * @throws SQLException
      */
     public void deleteAll() throws SQLException {
+        executeSql("delete from users"); // 변하는 sql 문장
+    }
+
+    private void executeSql(final String query) throws SQLException {
         // 메소드 파라미터로 이전한 익명 내부 클래스; DI받은 JdbcContext 의 컨텍스트 메소드를 사용하도록 변경한다.
         this.jdbcContext.workWithStatementStrategy(
+            // 변하지 않는 콜백 클래스 정의와 오브젝트 생성
             new StatementStrategy() {// 익명 내부 클래스는 구현하는 인터페이스를 생성자처럼 이용해서 오브젝트로 만든다.
                 public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement("delete from users");
+                    PreparedStatement ps = c.prepareStatement(query);
                     return ps;
                 }
             }
