96f76b210631489bce29da1f768a761f0e59b646
mitsui3point <rladbsgh0331@gmail.com>
2022-06-24 12:35:45 +0900

    3.6. 스프링의 JdbcTemplate 3.6.1. update()

--- src/main/java/springbook/user/dao/UserDao.java
+++ src/main/java/springbook/user/dao/UserDao.java
@@ -2,6 +2,8 @@ package springbook.user.dao;
 
 
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import springbook.user.domain.User;
 
 import javax.sql.DataSource;
@@ -14,40 +16,27 @@ public class UserDao {
 
     // UserDao 모든 메소드가 JdbcContext 를 사용하지 않으므로, 기존방법을 사용해서 동작하는 메소드를 위해 UserDao 가 아직은 DataSource 를 DI 받아야 함.
     private DataSource dataSource;
-    // 수정자 메서드를 이용하여 생성자 DI 를 대체
-    /*
-    JdbcContext 를 DI 받도록 만든다. => JdbcContext 가 Spring 에 의해 DI 받지 않고, UserDao 의 setDataSource 에 의해 DI 받는다.
-     */
-    private JdbcContext jdbcContext;
 
+    private JdbcTemplate jdbcTemplate;
+
+    // 수정자 메서드를 이용하여 생성자 DI 를 대체
     public void setDataSource (DataSource dataSource) { // 수정자 메소드이면서 JdbcContext 에 대한 생성, DI 작업을 동시에 수행한다.
-        this.jdbcContext = new JdbcContext(); // JdbcContext 생성(IoC)
-        this.jdbcContext.setDataSource(dataSource); // 의존 오브젝트 주입(DI)
+        this.jdbcTemplate = new JdbcTemplate(); // JdbcTemplate 생성(IoC)
+        this.jdbcTemplate.setDataSource(dataSource); // 의존 오브젝트 주입(DI)
         this.dataSource = dataSource; // 아직 JdbcContext 를 적용하지 않은 메소드를 위해 저장해둔다.
     }
 
     /**
-     * 클라이언트 책임을 담당할 add() 메소드
-     * add() 클라이언트 책임 1. 선정한 전략 클래스의 오브젝트 생성 : AddStatement 생성시 user 정보를 AddStatement 에 전달
-     * add() 클라이언트 책임 2. 컨텍스트 호출. 전략 오브젝트 전달
+     * JdbcTemplate 을 add() 메소드
+     * jdbcTemplate.update() 메소드 두개 중 sql, Object... 을 받는 메소드를 사용
      * @param user  AddStatement 전략 클래스에서 필요로 하는 추가 정보
      * @throws SQLException
      */
-    public void add(final User user) throws SQLException { //
-        // 메소드 파라미터로 이전한 익명 내부 클래스; DI받은 JdbcContext 의 컨텍스트 메소드를 사용하도록 변경한다.
-        this.jdbcContext.workWithStatementStrategy(
-            new StatementStrategy() { // 익명 내부 클래스는 구현하는 인터페이스를 생성자처럼 이용해서 오브젝트로 만든다.
-                public PreparedStatement makePreparedStatement(Connection c) throws SQLException {
-                    PreparedStatement ps = c.prepareStatement(
-                            "insert into users(id, name, password) values(?, ?, ?)");
-                    // 로컬(내부) 클래스의 코드에서 외부의 메소드 로컬 변수에 직접 접근할 수 있다. 단 외부의 메소드 로컬 변수를 사용하려면 외부 메소드 argument 에 final을 붙여주어야 한다.
-                    // user 파라미터는 메소드 내부에서 변경될 일이 없으므로 final로 선언해도 무방하다.
-                    ps.setString(1, user.getId());
-                    ps.setString(2, user.getName());
-                    ps.setString(3, user.getPassword());
-                    return ps;
-                }
-            }
+    public void add(final User user) throws SQLException {
+        this.jdbcTemplate.update("insert into users(id, name, password) values(?, ?, ?)",
+                user.getId(),
+                user.getName(),
+                user.getPassword()
         );
     }
 
@@ -81,14 +70,12 @@ public class UserDao {
     }
 
     /**
-     * 클라이언트 책임을 담당할 deleteAll() 메소드,
-     * JdbcContext로 옮긴 executeSql()을 사용하는 deleteAll() 메소드
-     * deleteAll 클라이언트 책임 1. 선정한 전략 클래스의 오브젝트 생성
-     * deleteAll 클라이언트 책임 2. 컨텍스트 호출. 전략 오브젝트 전달
+     * JdbcTemplate 을 적용한 deleteAll() 메소드
+     * jdbcTemplate.update() 메소드 두개 중 sql 을 받는 메소드를 사용
      * @throws SQLException
      */
     public void deleteAll() throws SQLException {
-        this.jdbcContext.executeSql("delete from users"); // 변하는 sql 문장
+        this.jdbcTemplate.update("delete from users");
     }
 
     public int getCount() throws SQLException {
